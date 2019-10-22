4c010e6b59c12d3d5fe8cedb82f2e10a7c9947a7
David Harrison <33928072+GibonKing@users.noreply.github.com>
2019-10-22 10:33:19 +0100

    Add files via upload

---
+++ FuSM/FuSMclass.h
@@ -0,0 +1,62 @@
+/* Copyright (C) Eric Dybsand, 2001. 
+ * All rights reserved worldwide.
+ *
+ * This software is provided "as is" without express or implied
+ * warranties. You may freely copy and compile this source into
+ * applications you distribute provided that the copyright text
+ * below is included in the resulting source code, for example:
+ * "Portions Copyright (C) Eric Dybsand, 2001"
+ */
+// FuSMclass.h: interface for the FuSMclass class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_FUSMCLASS_H__7041B021_E922_11D4_8C04_0080C8FE83CE__INCLUDED_)
+#define AFX_FUSMCLASS_H__7041B021_E922_11D4_8C04_0080C8FE83CE__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+// forward reference
+class FuSMstate;
+//
+// this is the declaration of the generic fuzzy state machine class
+//
+
+// Create a Standard Template Library <map> for containing the states
+//
+// NOTE: if STL is not supported on your system, then I apologize for
+// this inconvenience.  You will need to substitute its usage with a
+// similar container class.  The STL <map> is an indexable hash table
+// lookup container class, that provides fast access of generic objects,
+// and since STL is a standardized container class on most compilers,
+// I felt it would provide the best convenient container.
+//
+
+/*typedef HERE*/
+
+// Create a Standard Template Library <list> for membership states 
+//
+
+/*typedef HERE*/
+
+class FuSMclass  
+{
+	/* "SOMETHING" GOES HERE*/
+
+public:
+	FuSMclass();
+	~FuSMclass();
+
+	int GetInput(void) { return m_iCurrentInput; }
+
+	FuSMstate *GetState( int iStateID );	// return the FuSMstate object pointer
+	void AddState( FuSMstate *pState );	// add a FuSMstate object pointer to the map
+	void DeleteState( int iStateID );	// delete a FuSMstate object pointer from the map
+
+	void StateTransition( int iInput );	// perform fuzzy state transitions based on input
+	FuSMstate *GetNextFuzzyStateMember(void); // allow access to member fuzzy states
+};
+
+#endif // !defined(AFX_FUSMCLASS_H__7041B021_E922_11D4_8C04_0080C8FE83CE__INCLUDED_)
